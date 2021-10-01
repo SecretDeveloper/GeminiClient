6f15e2398b26829bac23c97e708bd8ef93485ceb
Gary Kenneally <GaryKenneally@gmail.com>
2021-10-01 15:37:23 +0100

    A little refactoring

---
+++ GeminiConsole.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Linq;
+
+namespace GeminiClient
+{
+  class GeminiConsole
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        if (args.Length == 0 || args[0] == "--help")
+        {
+          Console.WriteLine("Gemini user agent");
+          Console.WriteLine("Usage:");
+          Console.WriteLine("\tgemini url");
+          Console.WriteLine("\tGets a gemini document from the provided URL.");
+          return;
+        }
+
+        using(var client = new GeminiClient())
+        {
+          // Load the certificate
+          var certificate = LoadX509Certificate();
+
+          var uri = new Uri(args[0]);
+
+          // Fetch the URL
+          var response = client.Fetch(uri, certificate);
+
+          Console.WriteLine(response);
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("ERROR: " + ex.Message);
+        if (ex.InnerException != null)
+          Console.WriteLine("INNER ERROR: " + ex.InnerException.Message);
+      }
+    }
+
+    static X509Certificate LoadX509Certificate(string certpath = "GeminiClient.crt")
+    {
+      return new X509Certificate2(certpath);
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # Gemini Client
 
-
 ## Steps to generate X509 certificate
  - Generate a Private Key
-   - openssl genrsa -des3 -out server.key 1024
+   - openssl genrsa -des3 -out GeminiClient.key 1024
  - Generate a self-signed CSR
-   - openssl req -new -key server.key -out server.csr
+   - openssl req -new -key GeminiClient.key -out GeminiClient.csr
  - Remove passphrase from Key
-   - cp server.key server.key.org
-   - openssl rsa -in server.key.org -out server.key
+   - cp GeminiClient.key GeminiClient.key.org
+   - openssl rsa -in GeminiClient.key.org -out GeminiClient.key
  - Generate a self-signed certificate
-   - openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+   - openssl x509 -req -days 365 -in GeminiClient.csr -signkey GeminiClient.key -out GeminiClient.crt
